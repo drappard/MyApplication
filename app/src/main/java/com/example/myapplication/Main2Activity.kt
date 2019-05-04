ee4769527ab29ced3d0a4e6f83560d0acd8d0521
Daniel Rappard <drappard@g.emporia.edu>
2019-05-03 20:26:46 -0500

    added and updated activities

---
+++ app/src/main/java/com/example/myapplication/Main2Activity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+
+
+}
